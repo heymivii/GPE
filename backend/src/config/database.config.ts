c28795a4cea5ed72c614c40868773c703c2fdc7e
cherfa_e <cherfaouimehdi@yahoo.com>
2025-08-01 15:17:28 +0200

    docker

---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,9 @@
+export default () => ({
+  database: {
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || 'postgres',
+    name: process.env.DB_NAME || 'skywalk_db',
+  },
+});
