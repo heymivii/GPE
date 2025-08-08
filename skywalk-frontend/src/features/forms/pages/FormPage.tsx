81e7da8e123a4ff662f7b3908e4bdb2338278621
CoulibalyT <93912213+CoulibalyT@users.noreply.github.com>
2025-08-08 17:37:58 +0200

    add forms

--- skywalk-frontend/src/features/forms/pages/FormPage.tsx
+++ skywalk-frontend/src/features/forms/pages/FormPage.tsx
@@ -1,6 +1,5 @@
 export default function FormPage() {
     return(
-
         <div className="min-h-[90vh] flex items-center justify-center bg-gradient-to-b from-white to-gray-100">
         Formulaire
     </div>
